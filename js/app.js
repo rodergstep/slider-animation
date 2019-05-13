8732e2f70bebaf0aa26db55167140b8f03f8d193
rodergstep <rodergstep@gmail.com>
2019-05-13 16:37:05 +0300

    added artboard

--- js/app.js
+++ js/app.js
@@ -1,32 +1,46 @@
-$(document).ready(function() {
-  $('#slider').on('init', function(e, slick) {
-      var $firstAnimatingElements = $('div.slide:first-child').find('[data-animation]');
-      doAnimations($firstAnimatingElements);    
+$(document).ready(function () {
+  $('#slider').on('init', function (e, slick) {
+    var $firstAnimatingElements = $('div.slide:first-child').find('[data-animation]');
+    doAnimations($firstAnimatingElements);
   });
-  $('#slider').on('beforeChange', function(e, slick, currentSlide, nextSlide) {
-            var $animatingElements = $('div.slide[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-            doAnimations($animatingElements);    
+  $('#slider').on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+    var $animatingElements = $('div.slide[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+    doAnimations($animatingElements);
   });
   $('#slider').slick({
-     autoplay: true,
-     autoplaySpeed: 7000,
-     dots: false,
-     fade: true
+    autoplay: true,
+    autoplaySpeed: 7000,
+    dots: false,
+    fade: true,
+    arrows: true
   });
   function doAnimations(elements) {
-      var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-      elements.each(function() {
-          var $this = $(this);
-          var $animationDelay = $this.data('delay');
-          var $animationType = 'animated ' + $this.data('animation');
-          $this.css({
-              'animation-delay': $animationDelay,
-              '-webkit-animation-delay': $animationDelay
-          });
-          $this.addClass($animationType).one(animationEndEvents, function() {
-              $this.removeClass($animationType);
-          });
+    var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    elements.each(function () {
+      var $this = $(this);
+      var $animationDelay = $this.data('delay');
+      var $animationType = 'animated ' + $this.data('animation');
+      $this.css({
+        'animation-delay': $animationDelay,
+        '-webkit-animation-delay': $animationDelay
       });
+      $this.addClass($animationType).one(animationEndEvents, function () {
+        $this.removeClass($animationType);
+      });
+    });
   }
+
+  // Inline popups
+  $('#zoom-popups').magnificPopup({
+    delegate: 'a',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+      beforeOpen: function () {
+        this.st.mainClass = this.st.el.attr('data-effect');
+      }
+    },
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+
 });
 //# sourceMappingURL=app.js.map
